3a14eaeca8bfb8ecde8e219fd8519a13f3b85c45
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-08-30 13:18:18 +0000

    update

--- JS_Basics/01_First_Steps_in_Coding/01_First_Steps_in_Coding_Exercise/03_DepositCalculator.js
+++ JS_Basics/01_First_Steps_in_Coding/01_First_Steps_in_Coding_Exercise/03_DepositCalculator.js
@@ -1,16 +1,16 @@
-function depositCalculator(input){
+function depositCalculator(input) {
     const depositAmount = Number(input[0]);
     const depositTime = Number(input[1]);
-    const annualInterestRate = (Number(input[2]))/100;
+    const annualInterestRate = (Number(input[2])) / 100;
     const monthInYear = 12
 
     const accruedInterest = depositAmount * annualInterestRate
     const oneMonthInterest = accruedInterest / monthInYear
 
-    let result = depositAmount + depositTime*oneMonthInterest
+    let result = depositAmount + depositTime * oneMonthInterest
     console.log(result)
 
 }
 
-depositCalculator(["200 ","3 ","5.7 "])
-depositCalculator(["2350","6 ","7 "])
+depositCalculator(["200 ", "3 ", "5.7 "])
+depositCalculator(["2350", "6 ", "7 "])
