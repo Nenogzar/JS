84c2c15ba67451786208666ef78e2b4cd366c206
Nenogzar <stoyan.naidenov@gmail.com>
2024-08-31 15:10:53 +0300

    update

--- JS_Basics/01_First_Steps_in_Coding/01_First_Steps_in_Coding_Exercise/09_FishTank.js
+++ JS_Basics/01_First_Steps_in_Coding/01_First_Steps_in_Coding_Exercise/09_FishTank.js
@@ -1,19 +1,19 @@
 function fishTank(input) {
-  const [fishTankLength, fishTankWidth, fishTankHeight, fishTankPercentage] =
-    input.map(Number);
-  const litter = 0.001; //  / 1000
-  // let aquariumVolume = fishTankLength * fishTankWidth * fishTankHeight;
-  // let volumeInLiters = aquariumVolume * litter;
-  // let occupiedSpace = 1 - fishTankPercentage * 0.01;
-  // let litersNeeded = volumeInLiters * occupiedSpace;
+    const [fishTankLength, fishTankWidth, fishTankHeight, fishTankPercentage] =
+        input.map(Number);
+    const litter = 0.001; //  / 1000
+    // let aquariumVolume = fishTankLength * fishTankWidth * fishTankHeight;
+    // let volumeInLiters = aquariumVolume * litter;
+    // let occupiedSpace = 1 - fishTankPercentage * 0.01;
+    // let litersNeeded = volumeInLiters * occupiedSpace;
 
-  let litersNeeded =
-    litter *
-    fishTankLength *
-    fishTankWidth *
-    fishTankHeight *
-    (1 - 0.01 * fishTankPercentage);
-  console.log(litersNeeded);
+    let litersNeeded =
+        litter *
+        fishTankLength *
+        fishTankWidth *
+        fishTankHeight *
+        (1 - 0.01 * fishTankPercentage);
+    console.log(litersNeeded);
 }
 
 fishTank(["85 ", "75 ", "47 ", "17 "]);
