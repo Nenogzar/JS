22a67c7c36e2b1326eaf2caebe8519fd6d0199f6
StoyanNaidenov <103338442+Nenogzar@users.noreply.github.com>
2024-09-02 08:40:32 +0000

    update

--- JS_Basics/02_Conditional_Statements/02_Conditional_Statements_Lab/demo.js
+++ JS_Basics/02_Conditional_Statements/02_Conditional_Statements_Lab/demo.js
@@ -1,14 +1,35 @@
 // let isTheSame = 6 !== 5;
 // console.log(isTheSame);
 
-let isRaining = true;
-if (isRaining) {
-    console.log("Get an umbrella");
-} else console.log("End of programs");
+// let isRaining = true;
+// if (isRaining) {
+//     console.log("Get an umbrella");
+// } else console.log("End of programs");
+
+// let num1 = 5;
+// let num2 = 4;
+
+// if (num1 > num2) {
+//     console.log("Number 1 is bigger");
+// } else console.log("Number 2 is bigger");
+
+// let number = 123.12555
+
+// console.log(number.toFixed(2))
+
 
 let num1 = 5;
-let num2 = 4;
+let num2 = 6;
+let nim3 = 3;
+let bigger = (
+    num1 > num2 ? "num1 is bigger":
+    "num2 is bigger"
+)
+console.log(bigger)
 
-if (num1 > num2) {
-    console.log("Number 1 is bigger");
-} else console.log("Number 2 is bigger");
+let fixedNumber = (
+    num1 < 5 ? "number is less than 5":
+        num1 > 5 ? "number is greater than 5":
+            "the number is equal to 5"
+)
+console.log(fixedNumber)
